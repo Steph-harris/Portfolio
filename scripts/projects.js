0ced6c1e9869c8d4b1033b863d459cf754747711
steph-harris <steph.harris21@gmail.com>
2015-12-04 01:40:26 -0500

    added projects js which uses Ajax to connect to github API for data

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,10 +1,27 @@
 $(document).ready(function(){
   $.ajax({
     type:"GET",
-    url:"https://api.github.com/users/steph-harris",
-    success: 
-    for(i=0; i<userInfo.length; i++){
-      
-    }
+    url:"https://api.github.com/users/Steph-harris/repos",
+    success: function (userInfo) {
+      // debugger;
+      for(var i=0; i<userInfo.length; i++){
+        var repoNms = repoLink(userInfo[i]);
+        // var newH2 = $("<h2>").append(repoNms);
+        $(".list-group").append(repoNms);
+      // .append(repoNms)
+      }
+    }  
   })
+
+  function repoLink (repoData) {
+    // create unique url for each repo
+    var repoUrl = repoData.html_url + "/commits";
+    // create a link to attach to each repo
+    var newLink = $("<a>")
+      .attr("href", repoUrl)
+      .addClass("list-group-item")
+      .append(repoData.name)
+
+    return newLink;
+  }   
 })
