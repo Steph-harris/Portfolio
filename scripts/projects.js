b2b4b45f844bfe70dc580766c104d4f58ad35b84
steph-harris <steph.harris21@gmail.com>
2015-12-03 21:33:17 -0500

    created AJAX function to grab my github repo info

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-  alert("yep");
+  $.ajax({
+    type:"GET",
+    url:"https://api.github.com/users/steph-harris",
+    success: 
+    for(i=0; i<userInfo.length; i++){
+      
+    }
+  })
 })
