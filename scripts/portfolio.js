ac4bfe35bede01991baf217d34a041ff0abbbf82
steph-harris <steph.harris21@gmail.com>
2015-12-05 22:39:48 -0500

    added test page with affixed navbar

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
-   // make navbar only appear from about me section
-   
+   // make navbar only affix from about me section
+   debugger
+ $('#sidebar').affix({
+    offset: {
+      top: 615
+    }
+  });
+
+  var $body   = $(document.body);
+  var homeHeight = $('#home').outerHeight(true) + 10;
+
+  $body.scrollspy({
+    target: '#leftCol',
+    offset: homeHeight
+  });        
 });
